04f044fbf59c9b0f6dfd544d9f3a43bfd68a18fa
Thiago Harada <thiharada2003@gmail.com>
2023-10-27 15:10:45 -0300

    AAAAAAA

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const supporters = [
     { name: 'Griaule', image: './images/partners/griaule.svg', url: 'https://griaule.com/' },
     { name: 'Poatek', image: './images/partners/poatek.png', url: 'https://poatek.com/' },
     { name: 'BCR.CX', image: './images/partners/bcrcx.png', url: 'https://www.bcrcx.com/' },
-    { name: 'EACH', image: './images/partners/each.svg', url: 'http://www5.each.usp.br/' },
+    //{ name: 'EACH', image: './images/partners/each.svg', url: 'http://www5.each.usp.br/' },
     { name: 'PET-SI', image: './images/partners/pet.png', url: 'http://www.each.usp.br/petsi/' }
 // ].sort((a, b) => a.name > b.name ? 1 : -1);
 ];
